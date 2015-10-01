a1654f060b19a322103b9d0155d5d22dd63dfd12
Daniel Perez <daniel@claudetech.com>
2015-10-01 20:49:46 +0900

    Fix extglob patterns and update tests.

--- test.js
+++ test.js
@@ -13,58 +13,61 @@ var isGlob = require('./');
 
 describe('isGlob', function () {
   it('should return `true` if it is a glob pattern:', function () {
-    isGlob('*.js').should.be.true;
-    isGlob('!*.js').should.be.true;
-    isGlob('!foo').should.be.true;
-    isGlob('!foo.js').should.be.true;
-    isGlob('**/abc.js').should.be.true;
-    isGlob('abc/*.js').should.be.true;
+    isGlob('*.js').should.be.true();
+    isGlob('!*.js').should.be.true();
+    isGlob('!foo').should.be.true();
+    isGlob('!foo.js').should.be.true();
+    isGlob('**/abc.js').should.be.true();
+    isGlob('abc/*.js').should.be.true();
   });
 
   it('should return `true` if the path has brace characters:', function () {
-    isGlob('abc/{a,b}.js').should.be.true;
-    isGlob('abc/{a..z}.js').should.be.true;
-    isGlob('abc/{a..z..2}.js').should.be.true;
+    isGlob('abc/{a,b}.js').should.be.true();
+    isGlob('abc/{a..z}.js').should.be.true();
+    isGlob('abc/{a..z..2}.js').should.be.true();
   });
 
   it('should return `true` if it has an extglob:', function () {
-    isGlob('abc/@(a).js').should.be.true;
-    isGlob('abc/!(a).js').should.be.true;
-    isGlob('abc/+(a).js').should.be.true;
-    isGlob('abc/*(a).js').should.be.true;
-    isGlob('abc/?(a).js').should.be.true;
+    isGlob('abc/@(a).js').should.be.true();
+    isGlob('abc/!(a).js').should.be.true();
+    isGlob('abc/+(a).js').should.be.true();
+    isGlob('abc/*(a).js').should.be.true();
+    isGlob('abc/?(a).js').should.be.true();
   });
 
-  it('should return `true` if it has extglob characters:', function () {
-    isGlob('abc/@.js').should.be.true;
-    isGlob('abc/!.js').should.be.true;
-    isGlob('abc/+.js').should.be.true;
-    isGlob('abc/*.js').should.be.true;
-    isGlob('abc/?.js').should.be.true;
+  it('should return `true` if it has extglob characters and is not valid path:', function () {
+    isGlob('abc/!.js').should.be.true();
+    isGlob('abc/*.js').should.be.true();
+    isGlob('abc/?.js').should.be.true();
+  });
+
+  it('should return `false` if it has extglob characters but is a valid path:', function () {
+    isGlob('abc/@.js').should.be.false();
+    isGlob('abc/+.js').should.be.false();
   });
 
   it('should return `true` if the path has regex characters:', function () {
-    isGlob('abc/(aaa|bbb).js').should.be.true;
-    isGlob('abc/?.js').should.be.true;
-    isGlob('?.js').should.be.true;
-    isGlob('[abc].js').should.be.true;
-    isGlob('[^abc].js').should.be.true;
-    isGlob('a/b/c/[a-z].js').should.be.true;
-    isGlob('[a-j]*[^c]b/c').should.be.true;
+    isGlob('abc/(aaa|bbb).js').should.be.true();
+    isGlob('abc/?.js').should.be.true();
+    isGlob('?.js').should.be.true();
+    isGlob('[abc].js').should.be.true();
+    isGlob('[^abc].js').should.be.true();
+    isGlob('a/b/c/[a-z].js').should.be.true();
+    isGlob('[a-j]*[^c]b/c').should.be.true();
   });
 
   it('should return `false` if it is not a string:', function () {
-    isGlob().should.be.false;
-    isGlob(null).should.be.false;
-    isGlob(['**/*.js']).should.be.false;
-    isGlob(['foo.js']).should.be.false;
+    isGlob().should.be.false();
+    isGlob(null).should.be.false();
+    isGlob(['**/*.js']).should.be.false();
+    isGlob(['foo.js']).should.be.false();
   });
 
   it('should return `false` if it is not a glob pattern:', function () {
-    isGlob('.').should.be.false;
-    isGlob('aa').should.be.false;
-    isGlob('abc.js').should.be.false;
-    isGlob('abc/def/ghi.js').should.be.false;
+    isGlob('.').should.be.false();
+    isGlob('aa').should.be.false();
+    isGlob('abc.js').should.be.false();
+    isGlob('abc/def/ghi.js').should.be.false();
   });
 });
 
