d03cc795dd411b3bb9f83693dd3aba1a2372bb31
doowb <brian.woodward@gmail.com>
2017-08-07 16:19:06 -0400

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * is-glob <https://github.com/jonschlinkert/is-glob>
  *
- * Copyright (c) 2014-2016, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 var isExtglob = require('is-extglob');
