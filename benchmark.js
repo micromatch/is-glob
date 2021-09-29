017a46a1e71cae81df794c3ffaa05cdb9b53b27f
Rich Trott <rtrott@gmail.com>
2021-09-28 22:03:03 -0700

    fixup! fix: improve performance

--- benchmark.js
+++ benchmark.js
@@ -5,19 +5,19 @@ if (!global.BigInt) {
   process.exit(0);
 }
 
-const assert = require('assert');
+var assert = require('assert');
 var isGlob = require('./');
 
 function runBenchmark(length) {
-  const start = process.hrtime.bigint();
-  ['(', '[', '{', '\\'].forEach((char) => {
+  var start = process.hrtime.bigint();
+  ['(', '[', '{', '\\'].forEach(function(char) {
     isGlob(char.repeat(length));
     isGlob(char.repeat(length), {strict: false});
   });
   return process.hrtime.bigint() - start;
 }
 
-const baseline = runBenchmark(1e6);
-console.log(`Benchmark took ${baseline / global.BigInt(1000000)} milliseconds.`);
+var baseline = runBenchmark(1e6);
+console.log('Benchmark took ' + baseline / global.BigInt(1000000) + ' milliseconds.');
 
 assert.ok(baseline < global.BigInt(1000000000), 'Benchmark goal is less than one second');
