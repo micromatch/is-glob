99ee5eb8b38cf3f83e7ca049e0def3b8f67c92c3
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2022-12-29 12:43:58 +0000

    refactor: refactoring the strictcheck module

--- utils/strictCheck.js
+++ utils/strictCheck.js
@@ -1,35 +1,34 @@
 module.exports = function strictCheck(str) {
-  var chars = { "{": "}", "(": ")", "[": "]" };
+  const chars = { "{": "}", "(": ")", "[": "]" };
+
   if (str[0] === "!") {
     return true;
   }
-  var index = 0;
-  var pipeIndex = -2;
-  var closeSquareIndex = -2;
-  var closeCurlyIndex = -2;
-  var closeParenIndex = -2;
-  var backSlashIndex = -2;
+
+  let index = 0;
+  let pipeIndex = -2;
+  let closeSquareIndex = -2;
+  let closeCurlyIndex = -2;
+  let closeParenIndex = -2;
+  let backSlashIndex = -2;
+
   while (index < str.length) {
+    // the string contains the * character
     if (str[index] === "*") {
       return true;
     }
 
+    // the string contains the ? character
     if (str[index + 1] === "?" && /[\].+)]/.test(str[index])) {
       return true;
     }
 
-    if (
-      closeSquareIndex !== -1 &&
-      str[index] === "[" &&
-      str[index + 1] !== "]"
-    ) {
-      if (closeSquareIndex < index) {
+    // the string contains the [] characters
+    if (str[index] === "[" && str[index + 1] !== "]") {
+      if (closeSquareIndex === -1 || closeSquareIndex < index) {
         closeSquareIndex = str.indexOf("]", index);
       }
       if (closeSquareIndex > index) {
-        if (backSlashIndex === -1 || backSlashIndex > closeSquareIndex) {
-          return true;
-        }
         backSlashIndex = str.indexOf("\\", index);
         if (backSlashIndex === -1 || backSlashIndex > closeSquareIndex) {
           return true;
@@ -37,13 +36,10 @@ module.exports = function strictCheck(str) {
       }
     }
 
-    if (
-      closeCurlyIndex !== -1 &&
-      str[index] === "{" &&
-      str[index + 1] !== "}"
-    ) {
+    // the string contains the {} characters
+    if (str[index] === "{" && str[index + 1] !== "}") {
       closeCurlyIndex = str.indexOf("}", index);
-      if (closeCurlyIndex > index) {
+      if (closeCurlyIndex !== -1 && closeCurlyIndex > index) {
         backSlashIndex = str.indexOf("\\", index);
         if (backSlashIndex === -1 || backSlashIndex > closeCurlyIndex) {
           return true;
@@ -52,14 +48,13 @@ module.exports = function strictCheck(str) {
     }
 
     if (
-      closeParenIndex !== -1 &&
       str[index] === "(" &&
       str[index + 1] === "?" &&
       /[:!=]/.test(str[index + 2]) &&
       str[index + 3] !== ")"
     ) {
       closeParenIndex = str.indexOf(")", index);
-      if (closeParenIndex > index) {
+      if (closeParenIndex !== -1 && closeParenIndex > index) {
         backSlashIndex = str.indexOf("\\", index);
         if (backSlashIndex === -1 || backSlashIndex > closeParenIndex) {
           return true;
@@ -67,8 +62,8 @@ module.exports = function strictCheck(str) {
       }
     }
 
-    if (pipeIndex !== -1 && str[index] === "(" && str[index + 1] !== "|") {
-      if (pipeIndex < index) {
+    if (str[index] === "(" && str[index + 1] !== "|") {
+      if (pipeIndex === -1 || pipeIndex < index) {
         pipeIndex = str.indexOf("|", index);
       }
       if (pipeIndex !== -1 && str[pipeIndex + 1] !== ")") {
@@ -83,19 +78,21 @@ module.exports = function strictCheck(str) {
     }
 
     if (str[index] === "\\") {
-      var open = str[index + 1];
-      index += 2;
-      var close = chars[open];
-
+      const open = str[index + 1];
+      const close = chars[open];
       if (close) {
-        var n = str.indexOf(close, index);
+        const n = str.indexOf(close, index + 2);
         if (n !== -1) {
           index = n + 1;
+          if (str[index] === "!") {
+            return true;
+          }
         }
-      }
-
-      if (str[index] === "!") {
-        return true;
+      } else {
+        index += 2;
+        if (str[index] === "!") {
+          return true;
+        }
       }
     } else {
       index++;
